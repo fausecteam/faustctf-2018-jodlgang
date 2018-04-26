b5fa87f4ae1858a7b7fdcb38221fcfc03a672855
Benedikt Lorch <benedikt.lorch@fau.de>
2018-04-25 23:46:09 -0400

    Build VGGFace using own CNN implementation. Verify equality of predictions compared to TensorFlow implementation

--- prototype/vggface/model.py
+++ prototype/vggface/model.py
@@ -1,80 +1,81 @@
-import tensorflow as tf
+from tensorwow.initializer import ConstantInitializer, TruncatedNormalInitializer
+from tensorwow.layers import ConvLayer, FullyConnectedLayer, MaxPoolLayer
+from tensorwow.functions import RectifiedLinearUnit, Softmax
+from collections import OrderedDict
 import numpy as np
 import h5py
-from vggface.ops import conv2d_relu, max_pool_2x2, weights_variable_truncated_normal, bias_variable
-from scipy.misc import imread
-from keras_vggface.utils import decode_predictions
 
 
 class VGGFace(object):
-    def __init__(self, sess):
-        self._sess = sess
-        self._images = tf.placeholder(tf.float32, [None, 224, 224, 3], name="images")
-        self._drop_rate = tf.placeholder(tf.float32, name="drop_rate")
-        self._output, self._output_logits = self.model(self._images, self._drop_rate)
+    def __init__(self):
+        self._input_height = 224
+        self._input_width = 224
+        self._input_channels = 3
 
-    @staticmethod
-    def model(input, drop_rate):
+        # Set up model
+
         # Block 1
         # (224, 224, 3) -> (112, 112, 64)
-        conv1_1 = conv2d_relu(input, 64, "conv1_1")
-        conv1_2 = conv2d_relu(conv1_1, 64, "conv1_2")
-        pool1 = max_pool_2x2(conv1_2)
+        conv1_1 = ConvLayer(3, self._input_channels, 64, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv1_2 = ConvLayer(3, 64, 64, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        pool1 = MaxPoolLayer(2, padding=0, stride=2)
 
         # Block 2
         # (112, 112, 64) -> (56, 56, 128)
-        conv2_1 = conv2d_relu(pool1, 128, "conv2_1")
-        conv2_2 = conv2d_relu(conv2_1, 128, "conv2_2")
-        pool2 = max_pool_2x2(conv2_2)
+        conv2_1 = ConvLayer(3, 64, 128, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv2_2 = ConvLayer(3, 128, 128, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        pool2 = MaxPoolLayer(2, padding=0, stride=2)
 
         # Block 3
         # (56, 56, 128) -> (28, 28, 256)
-        conv3_1 = conv2d_relu(pool2, 256, "conv3_1")
-        conv3_2 = conv2d_relu(conv3_1, 256, "conv3_2")
-        conv3_3 = conv2d_relu(conv3_2, 256, "conv3_3")
-        pool3 = max_pool_2x2(conv3_3)
+        conv3_1 = ConvLayer(3, 128, 256, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv3_2 = ConvLayer(3, 256, 256, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv3_3 = ConvLayer(3, 256, 256, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        pool3 = MaxPoolLayer(2, padding=0, stride=2)
 
         # Block 4
         # (28, 28, 256) -> (14, 14, 512)
-        conv4_1 = conv2d_relu(pool3, 512, "conv4_1")
-        conv4_2 = conv2d_relu(conv4_1, 512, "conv4_2")
-        conv4_3 = conv2d_relu(conv4_2, 512, "conv4_3")
-        pool4 = max_pool_2x2(conv4_3)
+        conv4_1 = ConvLayer(3, 256, 512, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv4_2 = ConvLayer(3, 512, 512, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv4_3 = ConvLayer(3, 512, 512, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        pool4 = MaxPoolLayer(2, padding=0, stride=2)
 
         # Block 5
         # (14, 14, 512) -> (7, 7, 512)
-        conv5_1 = conv2d_relu(pool4, 512, "conv5_1")
-        conv5_2 = conv2d_relu(conv5_1, 512, "conv5_2")
-        conv5_3 = conv2d_relu(conv5_2, 512, "conv5_3")
-        pool5 = max_pool_2x2(conv5_3)
+        conv5_1 = ConvLayer(3, 512, 512, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv5_2 = ConvLayer(3, 512, 512, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        conv5_3 = ConvLayer(3, 512, 512, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        pool5 = MaxPoolLayer(2, padding=0, stride=2)
 
-        flatten = tf.reshape(pool5, [-1, 7 * 7 * 512])
+        fc6 = FullyConnectedLayer(7 * 7 * 512, 4096, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        fc7 = FullyConnectedLayer(4096, 4096, RectifiedLinearUnit(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
+        fc8 = FullyConnectedLayer(4096, 2622, Softmax(), TruncatedNormalInitializer(mean=0, stddev=1e-2), ConstantInitializer(0))
 
-        with tf.variable_scope("fc6"):
-            fc6_weights = weights_variable_truncated_normal([7 * 7 * 512, 4096], mean=0, stddev=1e-2, name="fc6_weights")
-            fc6_bias = bias_variable([4096], value=0, name="fc6_bias")
-            fc6_z = tf.matmul(flatten, fc6_weights) + fc6_bias
-            fc6_a = tf.nn.relu(fc6_z)
-            fc6_dropout = tf.layers.dropout(fc6_a, rate=drop_rate)
+        self._layers = OrderedDict([
+            ("conv1_1", conv1_1),
+            ("conv1_2", conv1_2),
+            ("pool1", pool1),
+            ("conv2_1", conv2_1),
+            ("conv2_2", conv2_2),
+            ("pool2", pool2),
+            ("conv3_1", conv3_1),
+            ("conv3_2", conv3_2),
+            ("conv3_3", conv3_3),
+            ("pool3", pool3),
+            ("conv4_1", conv4_1),
+            ("conv4_2", conv4_2),
+            ("conv4_3", conv4_3),
+            ("pool4", pool4),
+            ("conv5_1", conv5_1),
+            ("conv5_2", conv5_2),
+            ("conv5_3", conv5_3),
+            ("pool5", pool5),
+            ("fc6", fc6),
+            ("fc7", fc7),
+            ("fc8", fc8),
+        ])
 
-        with tf.variable_scope("fc7"):
-            fc7_weights = weights_variable_truncated_normal([4096, 4096], mean=0, stddev=1e-2, name="fc7_weights")
-            fc7_bias = bias_variable([4096], value=0, name="fc7_bias")
-            fc7_z = tf.matmul(fc6_dropout, fc7_weights) + fc7_bias
-            fc7_a = tf.nn.relu(fc7_z)
-            fc7_dropout = tf.layers.dropout(fc7_a, rate=drop_rate)
-
-        with tf.variable_scope("fc8"):
-            fc8_weights = weights_variable_truncated_normal([4096, 2622], mean=0, stddev=1e-2, name="fc8_weights")
-            fc8_bias = bias_variable([2622], value=0, name="fc8_bias")
-            fc8_z = tf.matmul(fc7_dropout, fc8_weights) + fc8_bias
-            fc8_a = tf.nn.softmax(fc8_z)
-
-        return fc8_a, fc8_z
-
-    def load(self, weights_file):
-        self._sess.run(tf.global_variables_initializer())
-
+    def restore_weights(self, weights_file):
         with h5py.File(weights_file, "r") as f:
             layers = list(f.keys())
             for layer in layers:
@@ -87,45 +88,56 @@ class VGGFace(object):
                 for layer_parameter in layer_parameters:
                     trained_value = f[layer][layer_layers[0]][layer_parameter].value
 
-                    parameter_name = layer_parameter
-                    if parameter_name == "bias:0":
-                        parameter_name = "bias"
-                    elif parameter_name == "kernel:0":
-                        parameter_name = "weights"
-
-                    with tf.variable_scope(layer, reuse=True):
-                        tf_variable = tf.get_variable("{}_{}".format(layer, parameter_name))
-                        self._sess.run(tf.assign(tf_variable, trained_value))
-
-    def inference(self, imgs):
-        preprocessed_imgs = self.preprocess(imgs)
-
-        predictions = self._sess.run(self._output, feed_dict={self._images: preprocessed_imgs, self._drop_rate: 0})
-        return predictions
+                    if layer_parameter == "bias:0":
+                        self._layers[layer].bias = trained_value
+                    elif layer_parameter == "kernel:0":
+                        self._layers[layer].weights = trained_value
+                    else:
+                        raise ValueError("Unknown key")
 
     @staticmethod
-    def preprocess(imgs):
-        assert len(imgs.shape) == 4
+    def preprocess(images):
+        if not len(images.shape) == 4:
+            raise ValueError("Expected four-dimensional input")
 
-        preprocessed_imgs = np.copy(imgs)
+        preprocessed_imgs = np.copy(images)
         preprocessed_imgs = preprocessed_imgs[..., ::-1]
         preprocessed_imgs[..., 0] -= 93.5940
         preprocessed_imgs[..., 1] -= 104.7624
         preprocessed_imgs[..., 2] -= 129.1863
         return preprocessed_imgs
 
+    def inference(self, images):
+        if not len(images.shape) == 4:
+            raise ValueError("Expected four-dimensional input")
+        if images.shape[1] != self._input_height or images.shape[2] != self._input_width or images.shape[3] != self._input_channels:
+            raise ValueError("Invalid dimensions")
+
+        x = self.preprocess(images)
+        # Forward pass
+        for name, layer in self._layers.items():
+            print("Feeding through layer {}".format(name))
+            x = layer.forward(x)
+
+            # Flatten activations when transitioning from convolutional cascade to fully-connected layers
+            if name == "pool5":
+                num_samples = x.shape[0]
+                x = x.reshape(num_samples, -1)
+
+        # Return class probabilities after softmax activation
+        return x
+
 
 if __name__ == "__main__":
+    from scipy.misc import imread
+
     weights_file = "/home/explicat/.keras/models/vggface/rcmalli_vggface_tf_vgg16.h5"
-    with tf.Session() as sess:
-        cnn = VGGFace(sess)
-        cnn.load(weights_file)
+    cnn = VGGFace()
+    cnn.restore_weights(weights_file)
 
-        img_filename = "../keras-vggface/image/ajb.jpg"
-        img = imread(img_filename).astype(np.float)
-        # img = tf.keras.preprocessing.image.load_img("../keras-vggface/image/ajb.jpg", target_size=(224, 224))
-        # x = tf.keras.preprocessing.image.img_to_array(img)
+    img_filename = "../keras-vggface/image/ajb.jpg"
+    img = imread(img_filename).astype(np.float)
+    # img = tf.keras.preprocessing.image.load_img("../keras-vggface/image/ajb.jpg", target_size=(224, 224))
+    # x = tf.keras.preprocessing.image.img_to_array(img)
 
-        predictions = cnn.inference(img[None, :])
-        print(decode_predictions(predictions))
-
+    predictions = cnn.inference(img[None, :])
