7a519d274eed3e559b2ea42a72ec02b53568692c
Benedikt Lorch <benedikt.lorch@fau.de>
2018-05-03 11:12:12 -0400

    Add a few helper methods for conversion to TFRecords

--- prototype/final_dataset/convert_to_tfrecords.py
+++ prototype/final_dataset/convert_to_tfrecords.py
@@ -1,11 +1,102 @@
-from scipy.misc import imread
 import tensorflow as tf
-import numpy as np
 import argparse
 import random
 import json
 import os
 import re
+
+
+def _int64_feature(value):
+    """Wrapper for inserting int64 features into Example proto."""
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def _bytes_feature(value):
+    """
+    Wrapper for inserting bytes features into Example proto.
+    """
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def _convert_to_example(image_buffer, label):
+    """
+    Build an Example proto for an example.
+    """
+
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'label': _int64_feature(label),
+        'image': _bytes_feature(tf.compat.as_bytes(image_buffer))}))
+    return example
+
+
+class ImageCoder(object):
+    """Helper class that provides TensorFlow image coding utilities."""
+
+    def __init__(self):
+        # Create a single Session to run all image coding calls.
+        self._sess = tf.Session()
+
+        # Initializes function that converts PNG to JPEG data.
+        self._png_data = tf.placeholder(dtype=tf.string)
+        image = tf.image.decode_png(self._png_data, channels=3)
+        self._png_to_jpeg = tf.image.encode_jpeg(image, format="rgb", quality=100)
+
+        # Initializes function that decodes RGB JPEG data.
+        self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
+        self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
+
+    def png_to_jpeg(self, image_data):
+        return self._sess.run(self._png_to_jpeg, feed_dict={self._png_data: image_data})
+
+    def decode_jpeg(self, image_data):
+        image = self._sess.run(self._decode_jpeg, feed_dict={self._decode_jpeg_data: image_data})
+        assert len(image.shape) == 3
+        assert image.shape[2] == 3
+        return image
+
+
+def _is_png(filename):
+    """Determine if a file contains a PNG format image.
+
+    Args:
+      filename: string, path of the image file.
+
+    Returns:
+      boolean indicating if the image is a PNG.
+    """
+    return filename.endswith('.png')
+
+
+def _process_image(filename, coder):
+    """Process a single image file.
+
+    Args:
+      filename: string, path to an image file e.g., '/path/to/example.JPG'.
+      coder: instance of ImageCoder to provide TensorFlow image coding utils.
+    Returns:
+      image_buffer: string, JPEG encoding of RGB image.
+      height: integer, image height in pixels.
+      width: integer, image width in pixels.
+    """
+    # Read the image file.
+    with tf.gfile.FastGFile(filename, 'rb') as f:
+        image_data = f.read()
+
+    # Convert any PNG to JPEG's for consistency.
+    if _is_png(filename):
+        print('Converting PNG to JPEG for %s' % filename)
+        image_data = coder.png_to_jpeg(image_data)
+
+    # Decode the RGB JPEG.
+    image = coder.decode_jpeg(image_data)
+
+    # Check that image converted to RGB
+    assert len(image.shape) == 3
+    assert image.shape[2] == 3
+
+    return image_data
 
 
 def collect(data_dir, name_to_class_label_mapping_file, output_file):
@@ -25,24 +116,18 @@ def collect(data_dir, name_to_class_label_mapping_file, output_file):
 
     # Open TFRecords file
     writer = tf.python_io.TFRecordWriter(output_file)
+    coder = ImageCoder()
 
     for i in range(num_images):
         if i % 1000 == 0:
             print("Progress: {:d}/{:d}".format(i + 1, num_images))
 
         img_file = img_files[i]
-        img = imread(img_file)
-        if len(img.shape) == 2 or img.shape[2] == 1:
-            img = np.stack((img, img, img), axis=2)
-
         label = name_to_class_label_mapping[os.path.basename(os.path.dirname(img_file))]
-
-        # Create a feature
-        feature = {"train/label": tf.train.Feature(int64_list=tf.train.Int64List(value=[label])),
-                   "train/image": tf.train.Feature(bytes_list=tf.train.BytesList(value=[tf.compat.as_bytes(img.tostring())]))}
+        image_buffer = _process_image(img_file, coder)
 
         # Create an example protocol buffer
-        example = tf.train.Example(features=tf.train.Features(feature=feature))
+        example = _convert_to_example(image_buffer, label)
 
         # Serialize to string and write on the file
         writer.write(example.SerializeToString())
