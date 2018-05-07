9f112bb3418a357befe98c79d2676b6c07f8ca84
Lukas-Dresel <lukasdresel@ucsb.edu>
2018-05-07 14:37:46 -0700

    renamed

--- jodlgang/data_utils/celeb_labels.py
+++ jodlgang/data_utils/celeb_labels.py
@@ -6,6 +6,6 @@ def get_celeb_labels():
     with open(join(dirname(__file__), 'class_label_mapping.json')) as f:
         return json.load(f)
 
-def get_renamed_labels():
+def get_new_name_labels():
     with open(join(dirname(__file__), 'class_label_mapping_names.json')) as f:
         return json.load(f)
