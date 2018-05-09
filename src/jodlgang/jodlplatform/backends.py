Fix backend to expose the overflow vulnerability

src/jodlgang/jodlplatform/backends.py
@@ -14,31 +14,34 @@ class FaceAuthenticationBackend(object):
         if 'face_img' not in request.FILES:
             raise PermissionDenied
 
-        # Check the token and return the user
         try:
             user = User.objects.get(email=kwargs["username"])
         except User.DoesNotExist:
             raise PermissionDenied
 
+        logger.debug("Retrieving face recognition CNN")
+        cnn = get_face_recognition_cnn()
+
         try:
-            logger.debug("Retrieving face recognition CNN")
-            cnn = get_face_recognition_cnn()
             logger.debug("Converting image to numpy array")
             face_img = np.array(Image.open(request.FILES['face_img'])).astype(np.float)
-            logger.debug("Running inference")
+        except Exception as e:
+            logger.error("Exception in face recognition: {} ({})".format(str(e), type(e)))
+            raise PermissionDenied
+
+        if len(face_img.shape) != 3 or face_img.shape[0] != cnn.input_height or face_img.shape[1] != cnn.input_width or face_img.shape[2] != cnn.input_channels:
+            logger.info("Dimensions mismatch")
+            raise PermissionDenied
+
+        try:
             class_probabilities = cnn.inference(face_img[None, :])[0]
-
-            # TODO special handling for numpy exceptions
             most_likely_class = np.argmax(class_probabilities)
-
             if class_probabilities[most_likely_class] <= 0.5 or user.id != most_likely_class:
                 raise PermissionDenied
             return user
-
         except Exception as e:
             logger.error("Exception in face recognition: {} ({})".format(str(e), type(e)))
-            # TODO: remove this for the softmax overflow vuln!
-            raise PermissionDenied
+            PermissionDenied
 
     def get_user(self, user_id):
         try: