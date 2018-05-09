d16b8c67f981c6d3c068bcb2952df0e4b676b062
Benedikt Lorch <benedikt.lorch@fau.de>
2018-05-09 16:05:55 -0400

    Add constants for checker

---
+++ checker/jodlgang/constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+CRYPTO_LINGO = [
+    "FOMO",
+    "ATH",
+    "BEAR",
+    "WHALE",
+    "BEARWHALE",
+    "REKT",
+    "TO THE MOON",
+    "BUY THE DIP"
+    "ADDY",
+    "FUD",
+    "CHOYNA",
+    "ALTCOIN",
+    "ASHDRAKED",
+    "JOMO",
+    "PUMP AND DUMP",
+    "SHITCOIN",
+    "REVERSE INDICATOR"
+]
