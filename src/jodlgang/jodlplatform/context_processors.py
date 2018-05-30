e636a5e6660bb35f04dc2a5c352962f203bc4545
Benedikt Lorch <benedikt.lorch@fau.de>
2018-05-30 08:12:00 -0400

    Fix non-existing user server error when database is not accessible

--- src/jodlgang/jodlplatform/context_processors.py
+++ src/jodlgang/jodlplatform/context_processors.py
@@ -5,8 +5,9 @@ from .models import User
 
 def show_ambassador(request):
     team_id = getattr(settings, "TEAM_ID", None)
-    ambassador_user = User.objects.get(id=team_id)
-    if not ambassador_user:
+    try:
+        ambassador_user = User.objects.get(id=team_id)
+    except User.DoesNotExist:
         return {}
 
     return {"ambassador_name": ambassador_user.name, "ambassador_email": ambassador_user.email}
