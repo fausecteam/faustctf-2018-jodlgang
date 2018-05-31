970d19bd5537144021254ba0388b785e7315a492
Benedikt Lorch <benedikt.lorch@fau.de>
2018-05-31 13:33:46 +0200

    Clean up

--- webserver/postgres/01_set_up_database.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE jodlplatform;
-CREATE USER jodlgang WITH PASSWORD 'tothemoon';
-ALTER ROLE jodlgang SET client_encoding to 'utf8';
-ALTER ROLE jodlgang SET default_transaction_isolation TO 'read committed';
-ALTER ROLE jodlgang SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE jodlplatform TO jodlgang;
