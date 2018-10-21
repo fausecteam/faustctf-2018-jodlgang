497d1fb8f46dcf17bd5cc0b89f6ac5d279bff5d1
Benedikt Lorch <benedikt.lorch@fau.de>
2018-10-21 17:36:01 +0200

    Remove another directory used for prototyping

--- jodlgang/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import data_utils
-from . import client
-from . import tensorwow
-from . import image_utils
-from . import path_utils
--- jodlgang/checker/__init__.py
+++
@@ -1,9 +0,0 @@
-from os.path import join
-
-import os
-from jodlgang.path_utils import ExtraLoadPaths, SERVER_DIR
-
-with ExtraLoadPaths(os.path.abspath(os.path.join(os.path.basename(__file__), '..', 'checker', 'jodlgang'))):
-    import jodlgangchecker
-    import jodlgangclient
-    import constants
